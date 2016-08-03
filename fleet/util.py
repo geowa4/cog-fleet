e6ef1738afe33b1fb850b8da590697aefc20c665
George Adams, IV <g.w.adams.iv@gmail.com>
2016-08-03 13:44:57 -0400

    bad token param in api_get

--- fleet/util.py
+++ fleet/util.py
@@ -4,7 +4,7 @@ import requests_unixsocket
 FLEET_API = "http+unix://%2Fvar%2Frun%2Ffleet.sock/fleet/v1"
 
 
-def api_get(url, token, headers={}):
+def api_get(url, headers={}):
     url = "{}/{}".format(FLEET_API, url)
     with requests_unixsocket.monkeypatch():
         r = requests.get(url)
