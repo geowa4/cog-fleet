df4c2c7b053a1a8c93ffe91da7cefb6f6ac7b8a2
George Adams, IV <g.w.adams.iv@gmail.com>
2016-08-04 15:34:43 -0400

    get all units including systemd state
    The options field on the unit object was dropped since it was large and
    useless. A filter option was added since Cog does not handle sending
    large messages to Slack.

--- fleet/commands/unit.py
+++ fleet/commands/unit.py
@@ -12,9 +12,43 @@ class Unit(FleetBase):
 
     def list(self):
         results = []
-        for unit in util.api_get('units'):
-            results.append(unit)
+        try:
+            filter_string = self.request.options.get('filter', '')
+        except:
+            filter_string = ''
+        for unit in self.get_all_units():
+            if filter_string in unit.get('name'):
+                unit.pop('options', None)
+                results.append(unit)
         self.response.content(results).send()
+
+    def get_all_units(self):
+        units = []
+        fleet_response = util.api_get('units')
+        units.extend(fleet_response.get('units', []))
+        while fleet_response.get('nextPageToken', None) is not None:
+            fleet_response = util.api_get(
+                'units?nextPageToken={}'.format(
+                    fleet_response.get('nextPageToken')
+                )
+            )
+            units.extend(fleet_response.get('units', []))
+        self.amend_state_onto_units(units)
+        return units
+
+    def amend_state_onto_units(self, units):
+        for unit in units:
+            if unit.get('machineID') is None:
+                state = {}
+            else:
+                state = util.api_get(
+                    'state?machineID={}&unitName={}'.format(
+                        unit.get('machineID'), unit.get('name')
+                    )
+                ).get('states', [{}])[0]
+            unit['systemdSubState'] = state.get('systemdSubState', '')
+            unit['systemdLoadState'] = state.get('systemdLoadState', '')
+            unit['systemdActiveState'] = state.get('systemdActiveState', '')
 
     def parse_subcommand_(self):
         if self.request.args is None:
